5ff1b8b72e3f1952e9c638f4c30daed1afda42a1
boktop235 <nikitabokov235@mail.ru>
2026-05-10 11:08:38 +0300

    fix: resolve conflict in HomePage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -5,22 +5,22 @@ const HomePage = () => {
   const [state, setState] = useState('any');
 
   return (
-  <>
-    <h1>Каталог навыков</h1>
-     <h1>Каталог навыков</h1>
-    <RadioGroupUI
-      name="gender"
-      legend="Пол автора"
-      options={[
-        {value: "any", label: 'Не имеет значения'},
-        {value: "man", label: 'Мужской'},
-        {value: "woman", label: 'Женский'}
-      ]}
-      value={state}
-      onChange={setState}
-      direction="vertical"
-    />
-  </>
+    <>
+      <h1>Каталог навыков</h1>
+      <RadioGroupUI
+        name="gender"
+        legend="Пол автора"
+        options={[
+          { value: "any", label: 'Не имеет значения' },
+          { value: "man", label: 'Мужской' },
+          { value: "woman", label: 'Женский' }
+        ]}
+        value={state}
+        onChange={setState}
+        direction="vertical"
+      />
+    </>
   );
 };
+
 export default HomePage;
