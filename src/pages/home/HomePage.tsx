25b68e9924b893bd62a85612a57cf760c7131ac4
boktop235 <nikitabokov235@mail.ru>
2026-05-10 11:22:44 +0300

    fix: remove merge conflict markers

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { RadioGroupUI } from "../../shared/ui/RadioGroup/RadioGroup";
+
+const HomePage = () => {
+  const [state, setState] = useState('any');
+
+  return (
+    <>
+      <h1>Каталог навыков</h1>
+      <RadioGroupUI
+        name="gender"
+        legend="Пол автора"
+        options={[
+          { value: "any", label: 'Не имеет значения' },
+          { value: "man", label: 'Мужской' },
+          { value: "woman", label: 'Женский' }
+        ]}
+        value={state}
+        onChange={setState}
+        direction="vertical"
+      />
+    </>
+  );
+};
+
+export default HomePage;
