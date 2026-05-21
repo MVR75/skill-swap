8cfbf1d84e81fd26532b8fd80e48d9a31a1889eb
boktop235 <nikitabokov235@mail.ru>
2026-05-21 19:34:56 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import './app/globals.css';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from './shared/context/ThemeContext'; // 👈 Добавь импорт
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <ThemeProvider> {/* 👈 Добавь обертку ThemeProvider */}
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
